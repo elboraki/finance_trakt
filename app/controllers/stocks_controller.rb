557198947df14c8af6792f0efbe22437a8ce0905
elboraki <younes.elboraki@gmail.com>
2020-04-28 13:01:07 +0000

    add flash error message when enter wrong symbol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_from_search(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_from_search(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = 'You have entred wrong symbol'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = 'You have empty search'
+      redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,5 +3,7 @@ class Stock < ApplicationRecord
     StockQuote::Stock.new(api_key: 'pk_f779964c19e24d7aa8ffc428c87663c3')
     loocked_up_stock = StockQuote::Stock.quote(ticker_symbole)
     new(name: loocked_up_stock.company_name, ticker: loocked_up_stock.symbol, last_price: loocked_up_stock.latest_price)
+  rescue Exception => e
+    nil
   end
 end
