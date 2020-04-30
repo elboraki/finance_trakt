8fb4b23c100feb568de380b7f842f2c138d48843
elboraki <younes.elboraki@gmail.com>
2020-04-30 23:19:39 +0000

    strock restriction

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_search(ticker_symbole)
     StockQuote::Stock.new(api_key: 'pk_f779964c19e24d7aa8ffc428c87663c3')
     loocked_up_stock = StockQuote::Stock.quote(ticker_symbole)
