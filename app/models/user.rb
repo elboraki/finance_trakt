833940ea0efa158f3b1ac578639e04873bac8a48
elboraki <younes.elboraki@gmail.com>
2020-05-01 23:24:54 +0000

    listing friendship for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  has_many :friendships
+  has_many :friends, through: :friendships
   def stock_already_adedd?(ticker_symbol)
     stock = Stock.find_by_ticker(ticker_symbol)
     return false unless stock
@@ -22,10 +24,7 @@ class User < ApplicationRecord
   end
 
   def full_name
-    return "#{first_name} #{last_name}".strip if (first_name || last_name)
-    "anonymous"
+    return "#{first_name} #{last_name}".strip if first_name || last_name
+    'anonymous'
   end
-
-  
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'user/registrations' }
+  resources :users, only: [:show]
+  resources :friendships
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: %i[create destroy]
